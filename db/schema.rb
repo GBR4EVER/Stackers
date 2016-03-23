412c4ce2af137698b81a664e8d8fc2434cc39e94
Corey <burkleycorey@yahoo.com>
2016-03-22 22:57:58 -0500

    Generated badges scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322011126) do
+ActiveRecord::Schema.define(version: 20160323034943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
