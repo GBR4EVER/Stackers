5a11763ea2d7b8a967bdf92a7b8f3fc0a5d4ae5e
Corey <burkleycorey@yahoo.com>
2016-03-23 23:31:58 -0500

    Added name column to badges table. Also added some more stlying changes and working with updating badges with names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323034943) do
+ActiveRecord::Schema.define(version: 20160324041606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160323034943) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
