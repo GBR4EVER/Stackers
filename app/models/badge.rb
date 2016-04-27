73c994aaa7c1899bae965a4697c7f72707a5a9b6
Corey <burkleycorey@yahoo.com>
2016-04-27 10:05:03 -0500

    Checking the heroku config

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,11 @@
 class Badge < ActiveRecord::Base
   
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { 
+    :medium => "300x300>", 
+    :thumb => "100x100>",
+    :square => "200x200#"
+  }
+  
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
