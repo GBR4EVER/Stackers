f466652cbdb8e218df2e7442622ed85ee6206302
Corey <burkleycorey@yahoo.com>
2016-04-27 20:26:50 -0500

    Going to delete previous s3 bucket and create new bucket with new credentials to hopefully resolve aws paperclip storage issue in storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,12 +81,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'blooming-fjord-28332.herokuapp.com' }
   
   # aws-sdk storage destination for heroku.
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  
 end
